ace7fea574a317d5f6265e35aecd261d753a5e93
Alex Lewis <alexmichael.lewis@gmail.com>
2015-12-05 18:03:04 -0500

    hooked up to firebase for real time updates, including posts and deletes

--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -1,21 +1,54 @@
 import React from 'react';
 import mui from 'material-ui';
+import trim from 'trim';
+import Firebase from 'firebase';
 
 var {Card} = mui;
 
 class MessageBox extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      message: '',
+    };
+
+    this.firebaseRef = new Firebase('https://am-react-play.firebaseio.com/messages');
   }
+
+onChange(e) {
+  this.setState({
+    message: e.target.value,
+  });
+}
+
+onKeyUp(e) {
+  if (e.keyCode === 13 && trim(e.target.value) != '') {
+    e.preventDefault();
+
+    this.firebaseRef.push({
+      message: this.state.message,
+    });
+
+    this.setState({
+      message: '',
+    });
+
+    console.log('Sent a new message: ', e.target.value);
+  }
+}
 
   render() {
     return (
       <Card style={{
           maxWidth: 1200,
           margin: '30px auto',
-          padding: 30
+          padding: 30,
         }}>
-        <textarea style={{
+        <textarea
+          value={this.state.message}
+          onChange={this.onChange.bind(this)}
+          onKeyUp={this.onKeyUp.bind(this)}
+          style={{
             width: '100%',
             borderColor: '#D0D0D0',
             resize: 'none',
@@ -23,7 +56,7 @@ class MessageBox extends React.Component {
             minHeight: 50,
             color: '#555',
             fontSize: 14,
-            outline: 'auto 0px'
+            outline: 'auto 0px',
           }}/>
       </Card>
     );
--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Message from './Message.jsx';
 import mui from 'material-ui';
+import Firebase from 'firebase';
+import _ from 'lodash';
 
 var {Card, List} = mui;
 
@@ -8,24 +10,39 @@ class MessageList extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      messages: [
-        'Oh hi, how are you?',
-        'I am good, you?'
-      ]
+      messages: [],
     };
+
+    this.firebaseRef = new Firebase('https://am-react-play.firebaseio.com/messages');
+    this.firebaseRef.on('child_added', (msg) => {
+      if(this.state.messages[msg.key()]) {
+        return;
+      }
+      
+      let msgVal = msg.val();
+      msgVal.key = msg.key();
+      this.state.messages[msgVal.key] = msgVal;
+      this.setState({messages: this.state.messages});
+    });
+    
+    this.firebaseRef.on('child_removed', (msg) => {
+      var key = msg.key();
+      delete this.state.messages[key];
+      this.setState({messages: this.state.messages});
+    });
   }
 
   render() {
-    var messageNodes = this.state.messages.map((message) => {
+    var messageNodes = _.values(this.state.messages).map((message) => {
       return (
-        <Message message={message} />
+        <Message message={message.message} />
       );
     });
 
     return (
       <Card style={{
           flexGrow: 2,
-          marginLeft: 30
+          marginLeft: 30,
         }}>
         <List>
           {messageNodes}
